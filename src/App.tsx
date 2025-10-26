ea99ba8fa1abe343c5027dbc670ddd5e596ed6b9
Danika <dazcha@hotmail.com>
2025-10-26 22:50:24 +0000

    added navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,31 @@
+import { useState } from 'react';
 import Compare from './pages/Compare';
 import Learn from './pages/Learn';
 import Practice from './pages/Practice';
 
 function App() {
+  const [activeTab, setActiveTab] = useState('');
+
+  function onTabClick(section: string) {
+    setActiveTab(section);
+  }
+
   return (
     <>
       <h1>Side By Side Code</h1>
 
+      <nav>
+        <ul>
+          <li onClick={() => onTabClick('learn')}>Learn</li>
+          <li onClick={() => onTabClick('compare')}>Compare</li>
+          <li onClick={() => onTabClick('practice')}>Practice</li>
+        </ul>
+      </nav>
+
       <div>
-        <Learn />
-        <Compare />
-        <Practice />
+        {activeTab === 'learn' && <Learn />}
+        {activeTab === 'compare' && <Compare />}
+        {activeTab === 'practice' && <Practice />}
       </div>
     </>
   );
